628ba7afed2848daf7f59759e834b499cb357111
DJXJD <djxjd5713@gmail.com>
2024-01-25 01:32:00 -0500

    Created router with loadJS function

---
+++ js/router.js
@@ -0,0 +1,18 @@
+function loadJS(FILE_URL, async = true) {
+  let scriptEle = document.createElement("script");
+
+  scriptEle.setAttribute("src", FILE_URL);
+  scriptEle.setAttribute("type", "text/javascript");
+  scriptEle.setAttribute("async", async);
+
+  document.body.appendChild(scriptEle);
+
+  // success event
+  scriptEle.addEventListener("load", () => {
+    console.log("File loaded: " + FILE_URL)
+  });
+  // error event
+  scriptEle.addEventListener("error", (ev) => {
+    console.log("Error on loading file", ev);
+  });
+}
